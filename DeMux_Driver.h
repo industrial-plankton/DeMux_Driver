5cc4b8df77c550a787d9acda4339fc7fab6665ca
Cam <cam@industrialplankton.com>
2024-07-15 14:12:24 -0700

    init

---
+++ DeMux_Driver.h
@@ -0,0 +1,107 @@
+#ifndef _Demux_Driver_h_included
+#define _Demux_Driver_h_included
+
+#include <DigitalInput.h>
+#include <AnalogInput.h>
+#include <DigitalOutput.h>
+#include <stdint.h>
+
+// 74HC4067BQ,118 https://assets.nexperia.com/documents/data-sheet/74HC_HCT4067.pdf
+class DeMux_Driver
+{
+private:
+    DigitalOutput &S0;
+    DigitalOutput &S1;
+    DigitalOutput &S2;
+    DigitalOutput &S3;
+    DigitalInput &Common;
+
+public:
+    DeMux_Driver(DigitalOutput &S0,
+                 DigitalOutput &S1,
+                 DigitalOutput &S2,
+                 DigitalOutput &S3,
+                 DigitalInput &Common) : S0{S0}, S1{S1}, S2{S2}, S3{S3}, Common{Common} {};
+    ~DeMux_Driver(){};
+
+    PinState getState(uint8_t pinNumber)
+    {
+        if (pinNumber >= 16)
+        {
+            return floating;
+        }
+        S0.setState(static_cast<PinState>(pinNumber & 0b0001));
+        S1.setState(static_cast<PinState>(pinNumber & 0b0010));
+        S2.setState(static_cast<PinState>(pinNumber & 0b0100));
+        S3.setState(static_cast<PinState>(pinNumber & 0b1000));
+        // May need a delay here
+        return Common.getState();
+    }
+};
+
+class DeMuxedPin : public DigitalInput
+{
+private:
+    DeMux_Driver &demux;
+    uint8_t pinNumber;
+
+public:
+    DeMuxedPin(DeMux_Driver &demux, uint8_t pinNumber) : demux{demux}, pinNumber{pinNumber} {};
+    ~DeMuxedPin(){};
+    PinState getState() const
+    {
+        return demux.getState(pinNumber);
+    }
+};
+
+////////////////////////////////////////
+// Analog
+////////////////////////////////////////
+
+class DeMux_Driver_Analog
+{
+private:
+    DigitalOutput &S0;
+    DigitalOutput &S1;
+    DigitalOutput &S2;
+    DigitalOutput &S3;
+    AnalogInput &Common;
+
+public:
+    DeMux_Driver_Analog(DigitalOutput &S0,
+                        DigitalOutput &S1,
+                        DigitalOutput &S2,
+                        DigitalOutput &S3,
+                        AnalogInput &Common) : S0{S0}, S1{S1}, S2{S2}, S3{S3}, Common{Common} {};
+    ~DeMux_Driver_Analog(){};
+
+    int getState(uint8_t pinNumber)
+    {
+        if (pinNumber >= 16)
+        {
+            return floating;
+        }
+        S0.setState(static_cast<PinState>(pinNumber & 0b0001));
+        S1.setState(static_cast<PinState>(pinNumber & 0b0010));
+        S2.setState(static_cast<PinState>(pinNumber & 0b0100));
+        S3.setState(static_cast<PinState>(pinNumber & 0b1000));
+        // May need a delay here
+        return Common.getState();
+    }
+};
+
+class DeMuxedPin_Analog : public AnalogInput
+{
+private:
+    DeMux_Driver_Analog &demux;
+    uint8_t pinNumber;
+
+public:
+    DeMuxedPin_Analog(DeMux_Driver_Analog &demux, uint8_t pinNumber) : demux{demux}, pinNumber{pinNumber} {};
+    ~DeMuxedPin_Analog(){};
+    int getState() const
+    {
+        return demux.getState(pinNumber);
+    }
+};
+#endif
