a9e6be4f6472405029f67ad9a1e762e8e22897b0
Cam <cam@industrialplankton.com>
2024-09-24 16:29:08 -0700

    Bug: Fix pinstate casting to floating, delay does seem to be required so increase cache

--- DeMux_Driver.h
+++ DeMux_Driver.h
@@ -22,7 +22,7 @@ public:
                  DigitalOutput &S2,
                  DigitalOutput &S3,
                  DigitalInput &Common) : S0{S0}, S1{S1}, S2{S2}, S3{S3}, Common{Common} {};
-    ~DeMux_Driver(){};
+    ~DeMux_Driver() {};
 
     PinState getState(uint8_t pinNumber)
     {
@@ -30,11 +30,12 @@ public:
         {
             return floating;
         }
-        S0.setState(static_cast<PinState>(pinNumber & 0b0001));
-        S1.setState(static_cast<PinState>(pinNumber & 0b0010));
-        S2.setState(static_cast<PinState>(pinNumber & 0b0100));
-        S3.setState(static_cast<PinState>(pinNumber & 0b1000));
+        S0.setState(static_cast<PinState>(static_cast<bool>(pinNumber & 0b0001))); // Cast to bool prevent setting to floating
+        S1.setState(static_cast<PinState>(static_cast<bool>(pinNumber & 0b0010)));
+        S2.setState(static_cast<PinState>(static_cast<bool>(pinNumber & 0b0100)));
+        S3.setState(static_cast<PinState>(static_cast<bool>(pinNumber & 0b1000)));
         // May need a delay here
+        delay(1);
         return Common.getState();
     }
 };
@@ -49,10 +50,10 @@ private:
 
 public:
     DeMuxedPin(DeMux_Driver &demux, uint8_t pinNumber) : demux{demux}, pinNumber{pinNumber} {};
-    ~DeMuxedPin(){};
+    ~DeMuxedPin() {};
     PinState getState() const
     {
-        if (millis() - lastRead > 2)
+        if (millis() - lastRead > 50)
         {
             const_cast<DeMuxedPin *>(this)->cachedState = demux.getState(pinNumber);
             const_cast<DeMuxedPin *>(this)->lastRead = millis();
@@ -80,7 +81,7 @@ public:
                         DigitalOutput &S2,
                         DigitalOutput &S3,
                         AnalogInput &Common) : S0{S0}, S1{S1}, S2{S2}, S3{S3}, Common{Common} {};
-    ~DeMux_Driver_Analog(){};
+    ~DeMux_Driver_Analog() {};
 
     int getState(uint8_t pinNumber)
     {
@@ -88,11 +89,12 @@ public:
         {
             return floating;
         }
-        S0.setState(static_cast<PinState>(pinNumber & 0b0001));
-        S1.setState(static_cast<PinState>(pinNumber & 0b0010));
-        S2.setState(static_cast<PinState>(pinNumber & 0b0100));
-        S3.setState(static_cast<PinState>(pinNumber & 0b1000));
+        S0.setState(static_cast<PinState>(static_cast<bool>(pinNumber & 0b0001))); // Cast to bool prevent setting to floating
+        S1.setState(static_cast<PinState>(static_cast<bool>(pinNumber & 0b0010)));
+        S2.setState(static_cast<PinState>(static_cast<bool>(pinNumber & 0b0100)));
+        S3.setState(static_cast<PinState>(static_cast<bool>(pinNumber & 0b1000)));
         // May need a delay here
+        delay(1);
         return Common.getState();
     }
 };
@@ -103,14 +105,14 @@ private:
     DeMux_Driver_Analog &demux;
     uint8_t pinNumber;
     unsigned long lastRead = millis();
-    int cachedState = Low;
+    int cachedState = 0;
 
 public:
     DeMuxedPin_Analog(DeMux_Driver_Analog &demux, uint8_t pinNumber) : demux{demux}, pinNumber{pinNumber} {};
-    ~DeMuxedPin_Analog(){};
+    ~DeMuxedPin_Analog() {};
     int getState() const
     {
-        if (millis() - lastRead > 2)
+        if (millis() - lastRead > 100)
         {
             const_cast<DeMuxedPin_Analog *>(this)->cachedState = demux.getState(pinNumber);
             const_cast<DeMuxedPin_Analog *>(this)->lastRead = millis();
@@ -124,14 +126,21 @@ class DeMuxedPin_DigFromAnalog : public DigitalInput
 private:
     DeMux_Driver_Analog &demux;
     uint8_t pinNumber;
-    uint8_t threshold = 1;
+    int threshold = 1;
+    unsigned long lastRead = millis();
+    int cachedState = 0;
 
 public:
-    DeMuxedPin_DigFromAnalog(DeMux_Driver_Analog &demux, uint8_t pinNumber, uint8_t threshold) : demux{demux}, pinNumber{pinNumber}, threshold{threshold} {};
-    ~DeMuxedPin_DigFromAnalog(){};
+    DeMuxedPin_DigFromAnalog(DeMux_Driver_Analog &demux, uint8_t pinNumber, int threshold) : demux{demux}, pinNumber{pinNumber}, threshold{threshold} {};
+    ~DeMuxedPin_DigFromAnalog() {};
     PinState getState() const
     {
-        return static_cast<PinState>(demux.getState(pinNumber) > threshold);
+        if (millis() - lastRead > 100)
+        {
+            const_cast<DeMuxedPin_DigFromAnalog *>(this)->cachedState = demux.getState(pinNumber);
+            const_cast<DeMuxedPin_DigFromAnalog *>(this)->lastRead = millis();
+            return static_cast<PinState>(cachedState > threshold); // static_cast<PinState>(demux.getState(pinNumber) > threshold);
+        }
     }
 };
 #endif
